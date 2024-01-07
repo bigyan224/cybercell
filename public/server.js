05c8573cacc5a8fe2f94dec8c13c8210fb52ac0a
bigyan224 <bigyanacharya224@gmail.com>
2024-01-07 22:58:05 +0530

    gg

--- public/server.js
+++ public/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-const userModel = require("./models/user");
+const userModel = require("../models/user");
 var http = require('http');
 var path=require("path")
 const bodyParser = require('body-parser');
